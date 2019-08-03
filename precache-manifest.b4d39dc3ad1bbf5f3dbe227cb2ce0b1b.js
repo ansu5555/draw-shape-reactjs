6614253e286646949987f59c77fe7467dc7c8c7d
Ansuman <ansuman5555@gmail.com>
2019-08-03 12:03:14 +0530

    Updates

---
+++ precache-manifest.b4d39dc3ad1bbf5f3dbe227cb2ce0b1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ffaaee2eec3e173b042986435e2adef",
+    "url": "/draw-shape-reactjs/index.html"
+  },
+  {
+    "revision": "0b1c584efdc85b3c0b7c",
+    "url": "/draw-shape-reactjs/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "440ae375ac802b5ce362",
+    "url": "/draw-shape-reactjs/static/js/2.51175ea1.chunk.js"
+  },
+  {
+    "revision": "0b1c584efdc85b3c0b7c",
+    "url": "/draw-shape-reactjs/static/js/main.4f9bd664.chunk.js"
+  },
+  {
+    "revision": "672de1cb7f05d399a385",
+    "url": "/draw-shape-reactjs/static/js/runtime~main.a17fa82a.js"
+  }
+]);
